be28da5c5765bb6d80a6c3fc4444110cb84be306
jagili-lavanya <68577657+jagili-lavanya@users.noreply.github.com>
2021-06-28 12:19:56 +0530

    Add files via upload

---
+++ info.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Info(){
+    return(
+        <div class="note">
+        <h1>JavaScript and React.js</h1>
+        <p>A basic web ev React Js Bootcamp</p>
+        </div>   
+    );
+}
+export default Info; 
